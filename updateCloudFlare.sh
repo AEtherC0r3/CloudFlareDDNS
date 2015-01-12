5d1902ad9adba55c589d3b049af49dd866446ad0
Th3M4st3r <Th3M4st3r@users.noreply.github.com>
2015-01-12 16:37:09 +0200

    Added logging

--- updateCloudFlare.sh
+++ updateCloudFlare.sh
@@ -3,12 +3,14 @@
 ########## Edit the following lines ##########
 IPv4=true
 IPv6=false
+Logfile="/var/log/ddns.log"
 URL="https://www.example.com/updateCloudFlare.php?auth=***Insert Your Key Here***"
 ##############################################
 
+date >> $Logfile
 if $IPv4; then
-  wget -4qO- $URL &> /dev/null
+  wget -4qO- $URL >> $Logfile
 fi
 if $IPv6; then
-  wget -6qO- $URL &> /dev/null
+  wget -6qO- $URL >> $Logfile
 fi
